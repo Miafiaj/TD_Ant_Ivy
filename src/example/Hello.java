67e31e9462f624b689267e613ba3b2fb277f8805
Rouxel Jeremy <jrouxel@SA203-07A.ensi-bourges.fr>
2017-04-11 16:43:59 +0200

    Exercice 16

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -2,13 +2,51 @@
 package example;
 
 import org.apache.commons.lang.WordUtils;
+import java.util.*;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+
+
 
 public class Hello {
     public static void main(String[] args) {
         String  message = "hello ivy !";
         System.out.println("standard message : " + message);
-        System.out.println("capitalized by " + WordUtils.class.getName() 
-                                + " : " + WordUtils.capitalizeFully(message));
+        System.out.println("capitalized by " + WordUtils.class.getName() + " : " + WordUtils.capitalizeFully(message));
+        int monmax = 0;
+        String nb;
+        int temp;
+        try {
+        	CSVReader reader = new CSVReader(new FileReader("data.csv"));
+	        List<String[]> myEntries = reader.readAll();
+	        String[] test;
+	       	//parcourt
+	       	for (int i  = 0; i<myEntries.size(); i++)
+	       	{
+	       		test=myEntries.get(i);
+	       		nb=test[0];
+	       		temp=Integer.parseInt(nb);
+	       		System.out.println("Nombre lu :" + temp) ;
+	       		if (temp>monmax)
+	       		{
+	       			monmax=temp;
+	       		}
+	       	}
+	       	System.out.println("Max :" + monmax);
+	     } catch(IOException e) {
+        	e.printStackTrace();
+        }
     }
 }
         
