096aa086c9759fc5e3beb8118b88cfdf948f9627
Rouxel Jeremy <jrouxel@SA203-07A.ensi-bourges.fr>
2017-04-12 16:12:41 +0200

    Fin

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -1,52 +1,58 @@
 
 package example;
 
-import org.apache.commons.lang.WordUtils;
-import java.util.*;
+import java.io.*;
+import java.util.List;
 
 import com.opencsv.CSVReader;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
-
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
-
-
-
-
+/**
+ * Hello world!
+ *
+ */
 public class Hello {
-    public static void main(String[] args) {
-        String  message = "hello ivy !";
-        System.out.println("standard message : " + message);
-        System.out.println("capitalized by " + WordUtils.class.getName() + " : " + WordUtils.capitalizeFully(message));
-        int monmax = 0;
-        String nb;
+
+    private static CSVReader reader;
+
+	public static int max(int a, int b)
+    {
+    	return a > b ? a:b;
+    	//return a;
+    }
+    
+    public static void main( String[] args ) throws IOException
+    {
+    	//int result = max(4,5);
+        //System.out.println( "Max :" + result );
+    	int monmax=0;
+    	String nb;
         int temp;
-        try {
-        	CSVReader reader = new CSVReader(new FileReader("data.csv"));
-	        List<String[]> myEntries = reader.readAll();
-	        String[] test;
-	       	//parcourt
-	       	for (int i  = 0; i<myEntries.size(); i++)
-	       	{
-	       		test=myEntries.get(i);
-	       		nb=test[0];
-	       		temp=Integer.parseInt(nb);
-	       		System.out.println("Nombre lu :" + temp) ;
-	       		if (temp>monmax)
-	       		{
-	       			monmax=temp;
+    	try 
+    	{
+			reader = new CSVReader(new FileReader("data.csv"),',');
+			List<String[]>myEntries=reader.readAll();
+			String[] test = null;
+			for (int i =0; i<myEntries.size(); i++)
+			{
+	       			test=myEntries.get(i);				
+	       			//parcourt
+	       			for (int j  = 0; j<test.length; j++)
+	       			{
+	       				nb=test[j];
+	       				System.out.println("Nombre lu : " + nb);
+	       				temp=Integer.parseInt(nb);
+	       				if (max(monmax,temp)!=monmax)
+	       				{
+	       					monmax=temp;
+	       				}
+	       			}
 	       		}
-	       	}
 	       	System.out.println("Max :" + monmax);
-	     } catch(IOException e) {
-        	e.printStackTrace();
-        }
+    	} catch (FileNotFoundException e) 
+    		{
+				e.printStackTrace();
+			}
+    	
     }
 }
-        
+
